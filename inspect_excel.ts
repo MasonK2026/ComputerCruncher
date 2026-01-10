f19ae0740cf214540290bd282c07f45c6c56cfdd
Mason Kepka <mason.kepka26@scaschools.org>
2026-01-10 13:39:03 -0500

    Updates

--- inspect_excel.ts
+++
@@ -1,8 +0,0 @@
-import { readFile, utils } from 'xlsx';
-import { join } from 'path';
-
-const file = readFile(join(process.cwd(), '19AT Upload Test.xlsx'));
-const sheet = file.Sheets[file.SheetNames[0]];
-const data = utils.sheet_to_json(sheet, { header: 1 });
-console.log('Headers:', data[0]);
-console.log('First Row:', data[1]);
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/ComputerCruncher/", // <--- CHANGE THIS to match your repo name
-})
